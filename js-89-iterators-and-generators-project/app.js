c9b90cc52bafc99c62c4e378521e6ef26ac25c75
Alberuni Azad <azad.bbc@gmail.com>
2022-11-12 08:38:46 +0600

    Practice project completed on Iterator

--- js-89-iterators-and-generators-project/app.js
+++ js-89-iterators-and-generators-project/app.js
@@ -1,115 +1,83 @@
+// Get profiles from localStorage
+const profiles = JSON.parse(localStorage.getItem('users'));
+const $next    = document.querySelector('#next');
+
+if( !profiles ){
+
+    fetch('https://randomuser.me/api/?results=10')
+    .then( res => res.json() )
+    .then( data => {
+        localStorage.setItem('users', JSON.stringify(data.results));
+        let sData = JSON.parse(localStorage.getItem('users'));
+
+        return initIterator( sData );
+    })
+    .then( i => {
+        
+        var profile = i.next().value;
+        updateUI(profile);
+
+        $next.addEventListener('click', function(){
+            profile = i.next().value;
+            updateUI(profile);
+        });
+    } )
+} else {
+    var profileIterator = initIterator( profiles );
+    updateUI(profileIterator.next().value);
+
+    $next.addEventListener('click', function(){
+        updateUI(profileIterator.next().value);
+    });
+}
+
 /**
- * Plan
- * 
- * On load page for first time
- * request for the profiles data
- * save the data to local storage & update ui
- * 
- * user reload the webpage
- * get the data from localStorage & update ui
+ * Iterator
  */
+function initIterator( arr ){
+    var count = arr.length;
+    return {
+        next: function(){
+            if( count > 0 ){
+                count--;
 
-// resource https://stackoverflow.com/questions/70374293/how-to-iterate-using-iterator-over-api-object-using-javascript
+                return {
+                    done: false,
+                    value: arr[count]
+                }
+            } else {
+                return {
+                    done: true,
+                }
+            }
+        }
+    }
+}
 
-// document addEventListener('DOMContentLoaded')
+/**
+ * @param [profile=null] - The profile object that we want to display.
+ * 
+ * @returns void
+ */
+function updateUI( profile = null ){
 
-    // profile = getData();
+    if( profile == undefined ){
+        alert("No more profiles");
+        window.location.reload();
+        return;
+    }
 
+    // Iterator through the data and show the first user info then 2nd ans so on    
+    document.querySelector('#imageDisplay').innerHTML = `<img src="${profile.picture.large}" />`;
 
- // getData(){
-    // If data is set to local storage
-        // Get the data from localstorage 
-        // profile = profiles.next().value;
-        // return profile
-    // else
-        // Requst data from api and save it to local storage
-        // Get the data from localstorage 
-        // profile = profiles.next().value;
-        // return profile
-//  }   
-
-// const data = JSON.parse(localStorage.getItem('users'));
-
-// // For the first time take the data from API and save it to localstorage
-// var obj;
-
-// if( !data ){
-//     fetch('https://randomuser.me/api/?results=10')
-//     .then(res => res.json())
-//     .then(data => {
-//         obj = data;
-//     })
-//     .then(() => {
-//         // Set data to localstorage
-//         localStorage.setItem('users', JSON.stringify(obj.results));
-
-//         // updateUI( data[0] );
-//     });
-// } {
-//     // On load
-//     window.addEventListener('DOMContentLoaded', updateUI());
-
-//     document.querySelector('#next').addEventListener('click', updateUI);
-// }
-
-
-// function updateUI( profile = null ){
-
-//     console.log(!profile);
-//     const profiles = profileIterator( data );
-//     profile = profiles.next().value;
-
-//     if( !profile ){
-        
-//         // On click next button
-        
-//         console.log(profile);
-//     }
-
-//     if( profile == undefined ){
-//         alert("No more profiles");
-//         window.location.reload();
-//         return;
-//     }
-
-//     // Iterator through the data and show the first user info then 2nd ans so on    
-//     document.querySelector('#imageDisplay').innerHTML = `<img src="${profile.picture.large}" />`;
-
-//     let html = `
-//     <ul class="list-group">
-//         <li class="list-group-item">Name: ${profile.name.first} ${profile.name.last}</li>
-//         <li class="list-group-item">Age: ${profile.dob.age}</li>
-//         <li class="list-group-item">Email: ${profile.email}</li>
-//         <li class="list-group-item">Address: ${profile.location.city}, ${profile.location.country}</li>
-//         <li class="list-group-item">Phone: ${profile.phone}</li>
-//     </ul>
-//     `;
-//     document.querySelector('#profileDisplay').innerHTML = html;
-// }
-
-// /**
-//  * It takes an array and iterate through it
-//  * 
-//  * @param profiles - an array of profiles
-//  * @returns An object with a next method.
-//  */
-// function profileIterator( profiles ){
-//     let count = profiles.length;
-
-//     return {
-//         next: function(){
-//             if( count > 0 ){
-//                 count--;
-
-//                 return {
-//                     done: false,
-//                     value: profiles[count]
-//                 }
-//             } else {
-//                 return {
-//                     done: true,
-//                 }
-//             }
-//         }
-//     }
-// }
+    let html = `
+    <ul class="list-group">
+        <li class="list-group-item">Name: ${profile.name.first} ${profile.name.last}</li>
+        <li class="list-group-item">Age: ${profile.dob.age}</li>
+        <li class="list-group-item">Email: ${profile.email}</li>
+        <li class="list-group-item">Address: ${profile.location.city}, ${profile.location.country}</li>
+        <li class="list-group-item">Phone: ${profile.phone}</li>
+    </ul>
+    `;
+    document.querySelector('#profileDisplay').innerHTML = html;
+}
