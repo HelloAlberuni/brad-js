f23318d69d1376d4468d32dc0b8feb8d71322ede
Alberuni Azad <azad.bbc@gmail.com>
2022-11-30 08:39:46 +0600

    Practicing singleton pattern

---
+++ js-96-singleton-pattern/app.js
@@ -0,0 +1,16 @@
+/* 96. Sinngleton pattern
+======================================================= */
+
+
+/*
+/// Summary ///
+- Actually a menifestation of module pattern
+- Sinngleton object is a Immediate Anonymous function and can only return one instance of an object at a time
+- Repeated calls to the constructor will just return the same instance.
+- Like the module pattern, it maintains a private reference which nothing from the outsite can access
+- An example of using Sinngleton pattern is: one user object created at a time, maybe a logged in user
+- Sinngleton gives us a global point of access rather than embracing encapsulation
+- It is a well known pattern across all programming languages
+
+// From Sumit:
+*/
