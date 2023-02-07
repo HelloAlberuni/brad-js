708ee35493482686e8538e268b0702da4b0e7617
Alberuni Azad <azad.bbc@gmail.com>
2023-02-07 07:17:37 +0600

    Practice done on HTTP lib setup and Displaying posts

--- 113-121-project-microposts-using-babel-and-webpack/src/app.js
+++ 113-121-project-microposts-using-babel-and-webpack/src/app.js
@@ -14,5 +14,44 @@ import * as myallmodule from './module2'; // All modules are exported into the m
 
 // console.log(myallmodule.sayHello());
 
-import person3 from './module2'; // person3 module is exported as default in the file, so no need to use {person3}
-console.log(person3.name);
+// import person3 from './module2'; // person3 module is exported as default in the file, so no need to use {person3}
+// console.log(person3.name);
+
+import { http } from './http';
+
+// Get posts on dom load
+// document.addEventListener('DOMContentLoaded', function(){
+//     http.get('http://localhost:3000/posts')
+//     .then( res => console.log(res) )
+//     .catch( err => console.log(err) )
+// });
+
+// Post request
+// document.addEventListener('DOMContentLoaded', function(){
+//     http.post('http://localhost:3000/posts', {
+//         "title": "New post 1",
+//         "author": "azad"
+//     })
+//     .then( res => console.log(res) )
+//     .catch( err => console.log(err) )
+// });
+
+// Put request
+// document.addEventListener('DOMContentLoaded', function(){
+//     http.put('http://localhost:3000/posts/2', {
+//         "title": "New post title",
+//         "author": "azadd"
+//     })
+//     .then( res => console.log(res) )
+//     .catch( err => console.log(err) )
+// });
+
+import { ui } from './UI';
+
+// Delete request
+document.addEventListener('DOMContentLoaded', function(){
+    http.get('http://localhost:3000/posts')
+    .then( res => ui.displayPosts(res) )
+    // .then( res => console.log(res) ),
+    .catch( err => console.log(err) )
+});
