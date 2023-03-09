ea8f616b7d8cd7f7332b3d2107f68ce2266da2ec
Alberuni Azad <azad.bbc@gmail.com>
2023-03-09 06:07:25 +0600

    Added mockAPI endpoint for online demo

--- 113-121-project-microposts-using-babel-and-webpack/src/app.js
+++ 113-121-project-microposts-using-babel-and-webpack/src/app.js
@@ -1,59 +1,5 @@
-// CommonJS module syntax
-// const person = require('./module1');
-
-// ES2015 Module
-
-// Importing multiple modules
-// import { person, person2, sayHello } from './module2';
-// console.log(sayHello()); // need () for the function calling
-
-// Import all modules of a file
-import * as myallmodule from './module2'; // All modules are exported into the myallmodule alias/variable
-
-// console.log(myallmodule.person3.name); // leads error cause the person3 module exported in the file as defuault
-
-// console.log(myallmodule.sayHello());
-
-// import person3 from './module2'; // person3 module is exported as default in the file, so no need to use {person3}
-// console.log(person3.name);
-
 import { http } from './http';
 console.log(http);
-
-// Get posts on dom load
-// document.addEventListener('DOMContentLoaded', function(){
-//     http.get('http://localhost:3000/posts')
-//     .then( res => console.log(res) )
-//     .catch( err => console.log(err) )
-// });
-
-// Post request
-// document.addEventListener('DOMContentLoaded', function(){
-//     http.post('http://localhost:3000/posts', {
-//         "title": "New post 1",
-//         "author": "azad"
-//     })
-//     .then( res => console.log(res) )
-//     .catch( err => console.log(err) )
-// });
-
-// Put request
-// document.addEventListener('DOMContentLoaded', function(){
-//     http.put('http://localhost:3000/posts/2', {
-//         "title": "New post title",
-//         "author": "azadd"
-//     })
-//     .then( res => console.log(res) )
-//     .catch( err => console.log(err) )
-// });
-
-// Delete request
-// document.addEventListener('DOMContentLoaded', function(){
-//     http.delete('http://localhost:3000/posts/2')
-//     .then( res => console.log(res) )
-//     .catch( err => console.log(err) )
-// });
-
 
 import { ui } from './UI';
 
@@ -71,14 +17,14 @@ ui.posts.addEventListener('click', deletePost );
 
 // Display posts
 function displayPosts(){
-    http.get('http://localhost:3000/posts')
+    http.get('https://640875a02f01352a8a936fad.mockapi.io/posts')
     .then( res => ui.displayPosts(res) )
     .catch( err => console.log(err) )
 }
 
 // Reload post list when any post is added/edited/deleted
 function reloadPosts(){
-    http.get('http://localhost:3000/posts')
+    http.get('https://640875a02f01352a8a936fad.mockapi.io/posts')
     .then(res => ui.displayPosts(res))
     .then(err => console.log(err))
 }
@@ -96,7 +42,7 @@ function submitPost(e){
     
     if( !ui.idInput.value ){
         // Add
-        http.post('http://localhost:3000/posts', {
+        http.post('https://640875a02f01352a8a936fad.mockapi.io/posts', {
             title: ui.titleInput.value,
             body: ui.bodyInput.value
         })
@@ -108,7 +54,7 @@ function submitPost(e){
         .catch( err => console.log(err) )
     } else if( ui.idInput.value ){
         // Update
-        http.put(`http://localhost:3000/posts/${ui.idInput.value}`, {
+        http.put(`https://640875a02f01352a8a936fad.mockapi.io/posts/${ui.idInput.value}`, {
             title: ui.titleInput.value,
             body: ui.bodyInput.value
         })
@@ -145,7 +91,7 @@ function deletePost(e){
     if(e.target.parentElement.classList.contains('delete')){
         if( confirm('Do you want to delete the post!') ){
             let id = e.target.parentElement.dataset.id;
-            http.delete(`http://localhost:3000/posts/${id}`)
+            http.delete(`https://640875a02f01352a8a936fad.mockapi.io/posts/${id}`)
             .then( res => {
                 alert('Post Deleted!');
                 reloadPosts();
